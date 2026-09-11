a6b0978b254b5d86ca83e2903ae3d07838b5930d
vipul25006188-rgb <vipul.25006188@kiet.edu>
2026-09-11 18:31:24 +0530

    Add new Java file

---
+++ UseCase2.java
@@ -0,0 +1,53 @@
+abstract class Shape {
+
+    abstract double area();
+
+    void displayArea() {
+        System.out.println("Area = " + area());
+    }
+}
+
+class Circle extends Shape {
+
+    private double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double length;
+    private double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+public class UseCase2 {
+
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[2];
+
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+
+        for (Shape s : shapes) {
+            s.displayArea();
+        }
+    }
+}
